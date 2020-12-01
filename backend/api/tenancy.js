514a13608c602f32af7ce5ba80d5b47ca6ce86b9
VictoriaKush <victoria.v.kush@gmail.com>
2020-12-01 23:10:32 +0100

    added backend

---
+++ backend/api/tenancy.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const knex = require("../database");
+const router = express.Router();
+
+//Adds a new tenancy
+router.post("/", async (req, res) => {
+  const { addres, description, url, size, rooms } = req.body;
+  const newTenancy = { addres, description, url, size, rooms };
+
+  await knex("flats").insert(newTenancy);
+  res.send("New tenancy added");
+});
+
+//Returns Tenancy by id
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const tenancyById = await knex("flats").select().where({ id });
+  res.json(tenancyById);
+});
+
+// Updates the Tenancy by id
+router.put("/:id", async (req, res) => {
+  const { addres, description, url, size, rooms } = req.body;
+  const { id } = req.params;
+
+  await knex("flats")
+    .where({ id })
+    .update({ addres, description, url, size, rooms });
+  res.send(`Tenancy with id ${id} was updated`);
+});
+
+//Deletes the meal by id
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  await knex("flats").where({ id }).del();
+  res.send(`Tenancy with id ${id} was deleted`);
+});
+
+router.use((err, req, res, next) => {
+  res.status(400).send(err.message);
+});
+
+module.exports = router;
